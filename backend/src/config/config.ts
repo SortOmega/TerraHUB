e9e94b7197509b03569ca634012ae63ecb6b78e4
SortOmega <sortocarlo755@gmail.com>
2024-04-16 21:40:23 -0600

    feat: :sparkles: Se ha quitado validacion del puerto

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,11 +2,6 @@ import { InternalServerErrorException } from '@nestjs/common';
 import * as _ from 'lodash';
 import { PoolConfig } from 'pg';
 
-const port = Number(process.env.DB_PORT);
-if (Number.isInteger(port))
-  throw new InternalServerErrorException(
-    'Se ha pasado un puerto No Valido en la configuracion del API'
-  );
 export const dbConfig = () => {
   return {
     host: process.env.DB_HOST,
@@ -14,5 +9,5 @@ export const dbConfig = () => {
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-  };
+  } as PoolConfig;
 };
