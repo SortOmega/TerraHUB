126eea4c9db35d828c1bcb409d3a5c99ae711cc5
SortOmega <sortocarlo755@gmail.com>
2024-04-18 00:05:37 -0600

    feat: :sparkles: Incluido Modulo de Clientes en Modulo principal

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,15 @@ import { AppService } from './app.service';
 import { LoggerMiddleware } from './shared/middlewares/logger-middleware';
 import { PropiedadesModule } from './modules/propiedades/propiedades.module';
 import { DatabaseModule } from './shared/modules/database/database.module';
+import { ClientesModule } from './modules/clientes/clientes.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, PropiedadesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    PropiedadesModule,
+    ClientesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
